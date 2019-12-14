40f7dfd514341583c1f07010a9531b0e30fd0519
Elias Lankinen <elias@lankinen.xyz>
2019-12-14 17:04:05 +0200

    code runs

--- src/main.py
+++ src/main.py
@@ -7,5 +7,9 @@ config = setup_config(max_round=10,initial_stack=100,small_blind_amount=5)
 config.register_player(name="p1",algorithm=player.FishPlayer("call"))
 config.register_player(name="p2",algorithm=player.AlgorithmPlayer(algorithms.MCTS(1,True)))
 
-for _ in range(10):
+for _ in range(1000):
     game_result = start_poker(config, verbose=1)
+    print("----------")
+    print("----------")
+    print("----------")
+    print("----------")
